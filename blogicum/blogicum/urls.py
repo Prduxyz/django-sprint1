65892edca0b8e191d53c27dddad5a5af6c28e911
Aleksandr Naydanov <sunnyblur@gmail.com>
2026-03-10 23:31:59 +0700

    Исправил отступы в urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -20,9 +20,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
-    path('pages/', include('pages.urls', namespace='pages')),
+    path('admin/',
+         admin.site.urls),
+    path('',
+         include('blog.urls', namespace='blog')),
+    path('pages/',
+         include('pages.urls', namespace='pages')),
 ]
 urlpatterns += static(settings.STATIC_URL,
 document_root=settings.STATICFILES_DIRS[0])
