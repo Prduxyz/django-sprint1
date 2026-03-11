1d64f7dd984f0c4bc2d4a47a9410d5e5d5cd587c
Aleksandr Naydanov <sunnyblur@gmail.com>
2026-03-11 17:22:44 +0700

    review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+
 
 posts = [
     {
@@ -19,15 +21,7 @@ posts = [
         'date': '1 октября 1659 года',
         'category': 'not-my-day',
         'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
+                с мели приливом и пригнало гораздо ближе к берегу.''',
     },
     {
         'id': 2,
@@ -35,39 +29,28 @@ posts = [
         'date': '25 октября 1659 года',
         'category': 'not-my-day',
         'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
+                порывистый ветер.''',
     },
 ]
 
+posts_by_id = {post['id']: post for post in posts}
+
 
 def index(request):
-    template = 'blog/index.html'
-    context = {'posts': posts}  # передаём все посты
-    return render(request, template, context)
+    """Главная страница блога. Показывает все посты."""
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
 
 
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    category = [post for post in posts if post['category'] == category_slug]
-    context = {
-        'category_slug': category_slug,
-        'posts': category,
-    }
-    return render(request, template, context)
+    """Страница постов определённой категории."""
+    context = {'category_slug': category_slug}
+    return render(request, 'blog/category.html', context)
 
 
 def post_detail(request, id):
-    template = 'blog/detail.html'
-
-    post = None
-    for p in posts:
-        if p['id'] == id:
-            post = p
-            break
-
-    context = {'post': post}
-    return render(request, template, context)
+    """Страница отдельного поста."""
+    if id not in posts_by_id:
+        raise Http404('Пост не найден')
+    context = {'post': posts_by_id[id]}
+    return render(request, 'blog/detail.html', context)
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-
-from . import views
+from django.views.generic import TemplateView
 
 app_name = 'pages'
 
 urlpatterns = [
-    # Если вызван URL без относительного адреса (шаблон — пустые кавычки),
-    # то вызывается view-функция index() из файла views.py
-    path('about/', views.about, name='about'),
-    path('rules/', views.rules, name='rules'),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='pages/about.html'),
+        name='about',
+    ),
+    path(
+        'rules/',
+        TemplateView.as_view(template_name='pages/rules.html'),
+        name='rules',
+    ),
 ]
